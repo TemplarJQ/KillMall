cd3aafc59dab095c54a0f667f1c977e7ef5111ba
TemplarJQ <jqliu_bupt@163.com>
2019-07-09 23:19:33 +0800

    build maven project

---
+++ src/main/java/com/seckillmall/App.java
@@ -0,0 +1,13 @@
+package com.seckillmall;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
