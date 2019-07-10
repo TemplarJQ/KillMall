2745fda5b753419f00d0773fddff187a0bc11db2
TemplarJQ <jqliu_bupt@163.com>
2019-07-10 08:25:15 +0800

    fix lot of bugs

--- src/main/java/com/seckillmall/App.java
+++ src/main/java/com/seckillmall/App.java
@@ -1,7 +1,11 @@
 package com.seckillmall;
 
+import com.seckillmall.dao.UserDOMapper;
+import com.seckillmall.dataobject.UserDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +14,22 @@ import org.springframework.web.bind.annotation.RestController;
  *
  */
 
-@EnableAutoConfiguration
+@SpringBootApplication(scanBasePackages = {"com.seckillmall"})
 @RestController
+@MapperScan("com.seckillmall.dao")
 public class App 
 {
+    @Autowired
+    private UserDOMapper userDOMapper;
+
     @RequestMapping("/")
     public String home(){
-        return "Hello, World!";
+        UserDO userDO = userDOMapper.selectByPrimaryKey(1);
+        if(userDO == null){
+            return "用户不存在";
+        }else{
+            return userDO.getName();
+        }
     }
 
     public static void main( String[] args )
