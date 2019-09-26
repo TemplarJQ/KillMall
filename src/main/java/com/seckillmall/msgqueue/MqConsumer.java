3f1698444588ff9cdb00bf79ebeb1a40dccb75e7
TemplarJQ <jqliu_bupt@163.com>
2019-09-26 14:26:27 +0800

    add consumer process

--- src/main/java/com/seckillmall/msgqueue/MqConsumer.java
+++ src/main/java/com/seckillmall/msgqueue/MqConsumer.java
@@ -1,16 +1,22 @@
 package com.seckillmall.msgqueue;
 
+import com.alibaba.fastjson.JSON;
+import com.seckillmall.dao.ItemDOMapper;
+import com.seckillmall.dao.ItemStockDOMapper;
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
 import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.common.message.MessageExt;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class MqConsumer {
@@ -24,6 +30,9 @@ public class MqConsumer {
     @Value("${mq.topicname}")
     private String topicName;
 
+    @Autowired
+    private ItemStockDOMapper itemStockDOMapper;
+
     @PostConstruct
     public void init() throws MQClientException {
         consumer = new DefaultMQPushConsumer("stock_consumer_group");
@@ -33,8 +42,13 @@ public class MqConsumer {
         consumer.registerMessageListener(new MessageListenerConcurrently() {
             @Override
             public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> list, ConsumeConcurrentlyContext consumeConcurrentlyContext) {
-
-
+                //实现库存到数据库扣减的逻辑
+                Message message = list.get(0);
+                String jsonString  = new String(message.getBody());
+                Map<String, Object> map = JSON.parseObject(jsonString, Map.class);
+                Integer itemId = (Integer) map.get("itemId");
+                Integer amount = (Integer) map.get("amount");
+                itemStockDOMapper.decreaseStock(itemId, amount);
                 return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
             }
         });
